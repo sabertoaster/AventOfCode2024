b667251c66dc1bb83764d6e82b57a5fead52e550
sabertoaster <phidaotram123@gmail.com>
2024-12-03 17:09:27 +0700

    Day 3: Mull It Over
    Fixed day 2 code: Forked from pienapplechoco.
    Restructured input folder.
    Part 1 and Part 2 day 3 completed.

--- day2_Red_Nosed_Reports.cpp
+++ day2_Red_Nosed_Reports.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+/* #include <bits/stdc++.h>
 
 #define abs(a) ((a) > 0 ? (a) : -(a))
 #define sign(a) (a > 0 ? 1 : -1)
@@ -57,85 +57,7 @@ int process_normal(vector<int> &a)
 
 int process_tolerate(vector<int> &a)
 {
-    int size_ = a.size();
-    if (size_ < 2)
-    {
-        return 0;
-    }
-    int dir = sign(a[1] - a[0]), dis = abs(a[1] - a[0]);
-    int fl = 1, init = 1;
-    // std::cout << "\ncc " << a[1] << " " << a[0] << dir << " cc\n";
-    if (dir == 0)
-    {
-        fl--;
-    }
-    if (dis > 3 || sign(a[2] - a[1]) != dir)
-    {
-        fl--;
-        init = 2;
-        dir = sign(a[2] - a[1]);
-    }
-    if (fl < 0)
-    {
-        return 0;
-    }
-
-    if (dir < 0)
-    {
-        for (int i = init; i < size_ - 2; i++)
-        {
-    // cout << "CHECK " << fl << endl;
-            if (fl < 0)
-            {
-                return 0;
-            }
-            if (a[i + 1] - a[i] < -3 || a[i + 1] - a[i] >= 0)
-            {
-
-                int max_ = max(i + 1, size_ - 1);
-
-                if (a[max_] - a[i] < -3 || a[max_] - a[i] >= 0)
-                {
-                    return 0;
-                }
-                else
-                {
-                    fl--;
-                    i++;
-                }
-            }
-        }
-    }
-    else if (dir > 0)
-    {
-        for (int i = init; i < size_ - 2; i++)
-        {
-            if (fl < 0)
-            {
-                return 0;
-            }
-            if (a[i + 1] - a[i] > 3 || a[i + 1] - a[i] <= 0)
-            {
-                int max_ = max(i + 1, size_ - 1);
-
-                if (a[max_] - a[i] > 3 || a[max_] - a[i] <= 0)
-                {
-                    return 0;
-                }
-                else
-                {
-                    fl--;
-                    i++;
-                }
-            }
-        }
-    }
-    else
-    {
-        return 0;
-    }
-
-    return 1;
+    
 }
 
 // Create a read file function that get filename and read it into 2 vectors of in
@@ -180,4 +102,109 @@ int main()
     // cout << process_tolerate(a) << endl;
     cout << read_file_and_process("input_day2.txt", process_tolerate) << endl;
     return 0;
+} */
+
+// Disclaimer: My code is cooked while I'm trying to do O(n) and I'm too lazy to implement a new one so here's an alternative solution of @p1neapplechoco: https://github.com/p1neapplechoco/. Kudos to him.
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+int part1();
+int part2();
+
+bool isSafePart1(vector<int> &);
+bool isSafePart2(vector<int> &);
+
+int main()
+{
+    // cout << part1();
+    cout << part2();
+}
+
+int part1()
+{
+    int res = 0;
+    ifstream input("input.txt");
+    string line;
+
+    while (getline(input, line))
+    {
+        vector<int> nums;
+
+        for (int i = 0; i < line.size(); i++)
+        {
+            string tmp{};
+            while (line[i] != ' ')
+                tmp += line[i++];
+            nums.push_back(stoi(tmp));
+        }
+
+        if (isSafePart1(nums))
+            res++;
+    }
+
+    return res;
+}
+
+int part2()
+{
+    int res = 0;
+    ifstream input("input.txt");
+    string line;
+
+    while (getline(input, line))
+    {
+        vector<int> nums;
+
+        for (int i = 0; i < line.size(); i++)
+        {
+            string tmp{};
+            while (line[i] != ' ')
+                tmp += line[i++];
+            nums.push_back(stoi(tmp));
+        }
+
+        if (isSafePart2(nums))
+            res++;
+    }
+
+    return res;
+}
+
+bool isSafePart1(vector<int> &nums)
+{
+    if (nums[1] == nums[0])
+        return false;
+
+    bool isDescending = (nums[1] - nums[0] > 0);
+
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        int d = nums[i + 1] - nums[i];
+        bool check = d > 0;
+        if (abs(d) < 1 || abs(d) > 3 || check != isDescending)
+            return false;
+    }
+
+    return true;
+}
+
+bool isSafePart2(vector<int> &nums)
+{
+
+    if (isSafePart1(nums))
+        return true;
+    else
+        for (int i = 0; i < nums.size(); i++)
+        {
+            vector<int> tmp = nums;
+            tmp.erase(tmp.begin() + i);
+            if (isSafePart1(tmp))
+                return true;
+        }
+    return false;
 }
---
+++ day3_Mull_It_Over.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <regex>
+
+using namespace std;
+
+long long process_line(const string &line)
+{
+    long long sum = 0;
+    regex const pattern{R"~(mul\(-?\d+,-?\d+\))~"};
+    regex const numbers{R"~(\d+)~"};
+
+    sregex_iterator matches{line.begin(), line.end(), pattern};
+    sregex_iterator end;
+
+    for (; matches != end; ++matches)
+    {
+        string match = (*matches)[0].str();
+        cout << match << endl;
+        sregex_iterator nums{match.begin(), match.end(), numbers};
+        sum += std::stoll((*nums)[0]) * std::stoll((*++nums)[0]);
+    }
+    return sum;
+}
+
+long long process_line_p2(const string &line)
+{
+    long long sum_ = 0;
+    regex const pattern{R"~(mul\(-?\d+,-?\d+\)|do\(\)|don\'t\(\))~"};
+    regex const numbers{R"~(\d+)~"};
+
+    sregex_iterator matches{line.begin(), line.end(), pattern};
+    sregex_iterator end;
+    bool flag = true;
+    for (; matches != end; ++matches)
+    {
+        string match = (*matches)[0].str();
+        // cout << match << endl;
+        if (match == "do()")
+        {
+            flag = true;
+        }
+        else if (match == "don't()")
+        {
+            flag = false;
+        }
+        else if (flag) // if flag is true
+        {
+            sregex_iterator nums{match.begin(), match.end(), numbers};
+            // cout << std::stoll((*nums)[0]) << " " << std::stoll((*++nums)[0]) << endl;
+            sum_ += std::stoll((*nums)[0]) * std::stoll((*++nums)[0]);
+            // cout << match << endl;
+        }
+    }
+    return sum_;
+}
+
+int main()
+{
+    // process_line_p2(R"~(xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5)mul(8,5)))~");
+    long long total_sum = 0;
+    ifstream file("input/day3.txt");
+    string line;
+
+    while (getline(file, line))
+    {
+        total_sum += process_line_p2(line);
+    }
+    cout << "Total sum: " << total_sum << endl;
+
+    file.close();
+    return 0;
+}
